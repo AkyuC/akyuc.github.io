47e03ec314e4c2dfdb3672eef0e35162c41bd57d
AkyuC <937818146@qq.com>
2024-05-08 17:02:04 +0800

    Updated blog with current time: 2024-05-08_17-02-04

--- bin/autoPushInWin10.bat
+++ bin/autoPushInWin10.bat
@@ -1,6 +1,14 @@
 @echo off
 :: 获取当前日期和时间
-set DATESTR=%DATE:~10,4%-%DATE:~7,2%-%DATE:~4,2%_%TIME:~0,2%-%TIME:~3,2%-%TIME:~6,2%
+for /f "tokens=2 delims==" %%a in ('wmic OS Get localdatetime /value') do set "dt=%%a"
+set "YY=%dt:~0,4%"
+set "MM=%dt:~4,2%"
+set "DD=%dt:~6,2%"
+set "HH=%dt:~8,2%"
+set "MI=%dt:~10,2%"
+set "SS=%dt:~12,2%"
+set "DATESTR=%YY%-%MM%-%DD%_%HH%-%MI%-%SS%"
+
 
 :: 检查是否有未提交的改动
 git status
