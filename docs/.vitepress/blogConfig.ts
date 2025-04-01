e1a61979a790b69784a6f6b3bed65f7b27e64102
阿初 <34666393+AkyuC@users.noreply.github.com>
2025-04-01 15:17:50 +0800

    save

--- docs/.vitepress/blogConfig.ts
+++ docs/.vitepress/blogConfig.ts
@@ -5,7 +5,8 @@ export const sidebarBlog = [
       items: [
         { text: '简介', link: '/minimind/intro.html' },
         { text: '分词器', link: '/minimind/tokenizer.html' },
-        { text: '预训练', link: '/minimind/pretrain.html' }
+        { text: '预训练', link: '/minimind/pretrain.html' },
+        { text: '微调', link: '/minimind/fine-tuning.html' }
       ]
     }
 ]
